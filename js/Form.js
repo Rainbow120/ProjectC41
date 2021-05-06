c710e8a18a1a6dc8e94311af16f05159865f8cad
Rainbow120 <shreya.sahay012@gmail.com>
2021-05-06 16:28:38 +0530

    First Commit

--- js/Form.js
+++ js/Form.js
@@ -13,14 +13,14 @@ class Form{
     }
     display() {
         this.title.html("FRUIT CATCHER");
-        this.title.position(750, 35);
+        this.title.position(500, 35);
         this.title.style('font-size', '70px');
         this.title.style('color', 'skyblue');
-        this.input.position(900,400);
+        this.input.position(500,400);
         this.input.style('width', '200px');
         this.input.style('height', '20px');
         this.input.style('background', 'lavender');
-        this.button.position(905.5,500);
+        this.button.position(505.5,500);
         this.button.style('width', '200px');
         this.button.style('height', '40px');
         this.button.style('background', 'lightpink');
@@ -34,7 +34,7 @@ class Form{
             player.update();
             player.updateCount(playerCount);
             this.greeting.html("Hello " + player.name)
-            this.greeting.position(900,250);
+            this.greeting.position(500,250);
             this.greeting.style('color', 'white');
             this.greeting.style('font-size', '100px');
         });
